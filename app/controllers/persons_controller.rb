d1a7b0cef2389ea2e4d3e98f41909565b2388ba4
Ivan Khramtsovskiy <rus@5g.su>
2019-01-08 01:23:36 +0300

    Готово!

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -8,4 +8,25 @@ class PersonsController < ApplicationController
     @my_works = Specialist.where(:user_id => current_user, :done => 0)
     @specialists = Specialist.where(:order_id => @my_projects, :accepted => 0)
   end
+
+  def specialist
+    @specialist = Specialist.new(specialist_params)
+
+    respond_to do |format|
+      if @specialist.save
+        format.html { redirect_to user_root_path }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_specialist
+      @specialist = Specialist.new(specialist_params)
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def specialist_params
+      params.require(:specialist).permit(:order_id, :user_id)
+    end
 end
